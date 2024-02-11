8d79046558423c021a66f714071131b4c4c5b6f9
Prakash Jha <jhaprakash504@gmail.com>
2024-02-11 22:31:51 +0530

    Fix: Users Public Page

--- src/app/api/readpost/route.ts
+++ src/app/api/readpost/route.ts
@@ -1,43 +1,3 @@
-// import { NextResponse } from "next/server";
-// import prisma from "../../../../lib/prismadb";
-
-// export async function GET(res: NextResponse) {
-
-
-// 	try {
-
-// 		const post = await prisma.post.findMany({
-
-// 			select: {
-// 				author: {
-// 					select:{
-// 						name:true,
-// 						username:true,
-// 						image:true,
-// 						id:true
-// 					}
-// 				},
-// 				content: true,
-// 				title: true,
-// 				createdAt: true,
-// 				id: true,
-// 				draft: true,
-// 				publish: true,
-// 				coverPhoto:true
-// 			}
-// 			,
-// 			orderBy: {
-// 				createdAt: 'desc',
-// 			},
-// 		})
-
-
-// 		return NextResponse.json({ message: "Post Fetch successfully", post }, { status: 200 })
-// 	} catch (error) {
-// 		return NextResponse.json({ message: error }, { status: 400 })
-// 	}
-
-// }
 
 
 import { NextResponse } from "next/server";
@@ -45,7 +5,7 @@ import prisma from "@/lib/prismadb";
 import { Redis } from "ioredis";
 import { redisClient } from "@/lib/redis";
 
-export async function GET(res: NextResponse) {
+export async function GET() {
 
 
 	try {
